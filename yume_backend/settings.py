dedb7054118b46fe711bd0eafba9fd1838d35734
shankarrathod9632 <sakkurathod8585@gmail.com>
2026-02-19 22:04:15 +0530

    Deploy ready

--- yume_backend/settings.py
+++ yume_backend/settings.py
@@ -168,6 +168,20 @@ if not DEBUG:
 
 
 
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER") == "true":
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+
+
+
 
 
 # import dj_database_url
