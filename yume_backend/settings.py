5de30ba3316aa2a74ff5f87438d32e6bc463d12a
shankarrathod9632 <sakkurathod8585@gmail.com>
2026-02-19 21:32:58 +0530

    Deploy ready

--- yume_backend/settings.py
+++ yume_backend/settings.py
@@ -3,13 +3,6 @@ import os
 import dj_database_url
 
 
-
-if os.environ.get("CREATE_SUPERUSER"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
-
 # --------------------------------------------------
 # BASE DIRECTORY
 # --------------------------------------------------
